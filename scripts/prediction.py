ba8353131298d8ea561ac249e1ff82479eb1c7ba
Aaron Gebremaraim <aarongebremariam.94@gmail.com>
2025-01-13 17:18:59 +0300

    final toch

---
+++ scripts/prediction.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import pickle
+import pandas as pd
+from pandas import DataFrame
+
+
+class CropPredictor:
+    """Class for loading the model and predicting the crop yield"""
+
+    def __init__(self, model_path: str):
+        """
+          Initializes the CropPredictor class.
+        Args:
+            model_path (str): Path to the trained model file (pickle format)
+        """
+        self.model_path = model_path
+        self.model = None
+
+    def load_model(self):
+        """
+        Loads the trained model from the specified path.
+
+        Returns:
+            The loaded model.
+        """
+        print(f"Loading model from: {self.model_path}")
+        try:
+            with open(self.model_path, "rb") as file:
+                self.model = pickle.load(file)
+            print("Model loaded successfully")
+            print(f"Our model is: {self.model}")
+        except (FileNotFoundError, pickle.UnpicklingError) as e:
+            print(f"Error loading model: {e}")
+            raise
+
+    def predict_yield(self, input_data: DataFrame):
+        """
+        Makes predictions on the input data using the loaded model.
+
+        Args:
+            input_data: The input features for making predictions.
+
+        Returns:
+            The predicted values.
+        """
+        if self.model is None:
+            print("Model is not loaded. Please load the model first.")
+            return None
+
+        try:
+            predictions = self.model.predict(input_data)
+            return predictions
+        except (ValueError, AttributeError) as e:
+            print(f"Error during prediction: {e}")
+            raise
+
+
+if __name__ == "__main__":
+    predictor = CropPredictor(model_path="../models/random_forest_model.pkl")
+    predictor.load_model()
+
+    new_data = pd.DataFrame(
+        {
+            "elevation": [670.77900],
+            "latitude": [-14.742861],
+            "longitude": [-6.110221],
+            "location": [2],
+            "slope": [7.636470],
+            "rainfall": [1586.60],
+            "min_temperature_c": [-3.8],
+            "max_temperature_c": [33.4],
+            "ave_temps": [14.80],
+            "soil_fertility": [0.64],
+            "soil_type": [5],
+            "ph": [5.385873],
+            "pollution_level": [8.221326e-09],
+            "crop_type": [6],
+        }
+    )
+
+    test_predictions = predictor.predict_yield(new_data)
+    if test_predictions is not None:
+        print(f"Predictions: {test_predictions}")
